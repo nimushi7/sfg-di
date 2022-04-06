e76acbbf20bd4c1705b7fdbe9f98bb8689229b79
mateusz <mateuszjaroszewski@gmail.com>
2022-04-06 19:51:47 +0200

    default profile

--- src/main/java/pl/jones/mat/sfgdi/services/I18nPolishGreetingService.java
+++ src/main/java/pl/jones/mat/sfgdi/services/I18nPolishGreetingService.java
@@ -3,7 +3,7 @@ package pl.jones.mat.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PL")
+@Profile({"PL", "default"})
 @Service("i18nService")
 public class I18nPolishGreetingService implements GreetingService {
 
